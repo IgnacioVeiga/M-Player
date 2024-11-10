82a3a547d22ce37f0b47f089d275c1b09c39a67b
Ignacio <42973262+IgnacioVeiga@users.noreply.github.com>
2024-11-10 12:19:21 -0300

    frontend url

--- public/electron.js
+++ public/electron.js
@@ -17,7 +17,7 @@ function createWindow() {
     });
 
     mainWindow.loadURL(
-        process.env.ELECTRON_START_URL ||
+        'http://localhost:3000' ||
         `file://${path.join(__dirname, '../build/index.html')}`
     );
 }
