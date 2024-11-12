dd335449f2f21567345eb6096a5cc1b459680481
Ignacio Veiga <ignacio.veiga29@gmail.com>
2024-11-12 20:05:33 -0300

    New progressbar

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -7,9 +7,13 @@ export default function Controls({ file, onPrevious, onNext, onPlayPause, isPlay
     const { audioRef, progress, duration, setProgress } = useAudioPlayer(file, isPlaying);
 
     const handleProgressChange = (e) => {
-        const newTime = (e.target.value / 100) * duration;
-        audioRef.current.currentTime = newTime;
-        setProgress(newTime);
+        if (audioRef.current && duration && isFinite(duration)) {
+            const newTime = (e.target.value / 100) * duration;
+            if (!isNaN(newTime)) {
+                audioRef.current.currentTime = newTime;
+                setProgress(newTime);
+            }
+        }
     };
 
     const formatTime = (time) => {
@@ -19,54 +23,56 @@ export default function Controls({ file, onPrevious, onNext, onPlayPause, isPlay
     };
 
     return (
-        <footer>
-            <audio ref={audioRef} hidden />
-            <div className='controls-left'>
-                <button onClick={onPrevious}>
-                    <span className='material-icons'>skip_previous</span>
-                </button>
-                <button onClick={onPlayPause}>
-                    {
-                        isPlaying ?
-                            <span className='material-icons'>pause</span>
-                            :
-                            <span className='material-icons'>play_arrow</span>
-                    }
-                </button>
-                <button onClick={onNext}>
-                    <span className='material-icons'>skip_next</span>
-                </button>
-
-                <span className='time'>{formatTime(progress)} / {formatTime(duration)}</span>
-            </div>
-
-            <div className="metadata">
-                <Artwork file={file} size="thumbnail" />
-                <div className='metadata-text'>
-                    <span>{file?.title || 'Unknown Title'}</span>
-                    <span className='sub-title'>
-                        {file?.artist || 'Unknown Artist'} - {file?.album || 'Unknown Album'}
-                    </span>
-                </div>
-            </div>
-
+        <div className='controls'>
             <ProgressBar
                 progress={progress}
                 duration={duration}
                 onProgressChange={handleProgressChange}
             />
+            <audio ref={audioRef} hidden />
 
-            <div className='controls-right'>
-                <button>
-                    <span className='material-icons-outlined'>volume_up</span>
-                </button>
-                <button>
-                    <span className='material-icons'>repeat</span>
-                </button>
-                <button>
-                    <span className='material-icons'>shuffle</span>
-                </button>
-            </div>
-        </footer>
+            <footer>
+                <div className='controls-left'>
+                    <button onClick={onPrevious}>
+                        <span className='material-icons'>skip_previous</span>
+                    </button>
+                    <button onClick={onPlayPause}>
+                        {
+                            isPlaying ?
+                                <span className='material-icons'>pause</span>
+                                :
+                                <span className='material-icons'>play_arrow</span>
+                        }
+                    </button>
+                    <button onClick={onNext}>
+                        <span className='material-icons'>skip_next</span>
+                    </button>
+
+                    <span className='time'>{formatTime(progress)} / {formatTime(duration)}</span>
+                </div>
+
+                <div className="metadata">
+                    <Artwork file={file} size="thumbnail" />
+                    <div className='metadata-text'>
+                        <span>{file?.title || 'Unknown Title'}</span>
+                        <span className='sub-title'>
+                            {file?.artist || 'Unknown Artist'} - {file?.album || 'Unknown Album'}
+                        </span>
+                    </div>
+                </div>
+
+                <div className='controls-right'>
+                    <button>
+                        <span className='material-icons-outlined'>volume_up</span>
+                    </button>
+                    <button>
+                        <span className='material-icons'>repeat</span>
+                    </button>
+                    <button>
+                        <span className='material-icons'>shuffle</span>
+                    </button>
+                </div>
+            </footer>
+        </div>
     );
 }
--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,13 +1,20 @@
-import '../styles/ProgressBar.css'
+import '../styles/ProgressBar.css';
 
 export default function ProgressBar({ progress, duration, onProgressChange }) {
+    const progressPercentage = (progress / duration) * 100 || 0;
+
+    const handleProgressClick = (e) => {
+        const rect = e.target.getBoundingClientRect();
+        const clickX = e.clientX - rect.left;
+        const newTime = (clickX / rect.width) * duration;
+        onProgressChange({ target: { value: (newTime / duration) * 100 } });
+    };
+
     return (
-        <div className="progress-bar">
-            <progress
-                value={(progress / duration) * 100 || 0}
-                min="0" max="100"
-                onChange={onProgressChange}
-            />
+        <div className="progress-container" onClick={(e) => onProgressChange(e)}>
+            <div className="progress-bar" onClick={(e) => handleProgressClick(e)}
+                style={{ width: `${progressPercentage}%` }}></div>
+            <div className="progress-circle" style={{ left: `${progressPercentage}%` }}></div>
         </div>
     );
 }
