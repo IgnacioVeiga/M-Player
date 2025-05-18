e303dba9a52f3467d10bc1e04e0dd636b76fab28
Ignacio Veiga <ignacio.veiga29@gmail.com>
2025-05-17 23:10:30 -0300

    Add new SVG icon asset for application

--- src/electron/electron.js
+++ src/electron/electron.js
@@ -8,6 +8,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 860,
         height: 640,
+        // icon: path.join(__dirname, 'assets', 'icon.png'),
         webPreferences: {
             preload: path.join(__dirname, 'preload.js'),
             contextIsolation: true,
