e7af6db59aa9a5479ca75000a90a2d86d54e3ed2
Ignacio <42973262+IgnacioVeiga@users.noreply.github.com>
2025-05-23 19:10:46 -0300

    Refactor FileList: mejora la legibilidad del código y ajusta el estilo de los elementos de la lista

--- src/renderer/components/FileList.jsx
+++ src/renderer/components/FileList.jsx
@@ -7,19 +7,14 @@ export default function FileList({ files, file, onFileSelect }) {
 				<li
 					key={index}
 					onClick={() => onFileSelect(f)}
-					className={`flex justify-between items-center px-4 py-2 border-b border-neutral-800 cursor-pointer transition-colors
-            ${
-							file?.path === f.path
-								? "bg-neutral-800 font-medium border-l-4 border-primary"
-								: ""
-						}
-            hover:bg-neutral-700`}
-				>
+					className={`flex justify-between items-center px-4 py-2 border-y border-neutral-800 cursor-pointer transition-colors
+            					${file?.path === f.path
+								? "bg-neutral-800 font-medium border-l-primary border-2"
+								: ""} hover:bg-neutral-700`} >
+					
 					<div className="flex items-center gap-3">
-						<Artwork
-							file={f}
-							size="thumbnail"
-						/>
+						<Artwork file={f} size="thumbnail" />
+
 						<div className="flex flex-col">
 							<span>{f.title || f.name}</span>
 							<span className="text-xs text-neutral-400 truncate">
