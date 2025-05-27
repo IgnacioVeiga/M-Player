737687240912db0cb3c8bcece30a3a25565b9108
Ignacio Veiga <ignacio.veiga29@gmail.com>
2025-05-26 22:34:02 -0300

    Refactor: actualiza los estilos de carga con nuevos colores de fondo

--- electron/main/fileHandlers.ts
+++ electron/main/fileHandlers.ts
@@ -1,8 +1,10 @@
 import { AudioFile } from '@/types';
-import { ipcMain, dialog, OpenDialogReturnValue } from 'electron';
-import fs from 'fs';
-import mime from 'mime-types';
-import { IAudioMetadata, parseBuffer } from 'music-metadata';
+import { ipcMain, dialog, OpenDialogReturnValue, OpenDialogOptions } from 'electron';
+import { lookup } from 'mime-types';
+import { IAudioMetadata, IPicture, parseBuffer } from 'music-metadata';
+import { readFileSync } from 'fs'
+import { homedir } from 'os';
+import { join } from 'path';
 
 function uint8ArrayToBase64(uint8Array: Uint8Array): string {
     const binary: string = Array.from(uint8Array, byte => String.fromCharCode(byte)).join('');
@@ -11,25 +13,31 @@ function uint8ArrayToBase64(uint8Array: Uint8Array): string {
 
 export function registerFileHandlers() {
     ipcMain.handle('select-audio-files', async (): Promise<AudioFile[] | null> => {
-        const { canceled, filePaths }: OpenDialogReturnValue = await dialog.showOpenDialog({
+        const dialogOptions: OpenDialogOptions = {
             properties: ['openFile', 'multiSelections'],
-            filters: [{ name: 'Audio Files', extensions: ['mp3', 'wav', 'ogg'] }],
-        });
+            defaultPath: join(homedir(), 'Music'),
+            filters: [
+                { name: 'Audio Files', extensions: ['mp3', 'flac', 'ogg', 'm4a'] },
+            ],
+        };
+
+        // Show the open dialog to select audio files
+        const { canceled, filePaths }: OpenDialogReturnValue = await dialog.showOpenDialog(dialogOptions);
 
         if (canceled) return null;
 
         return await Promise.all(
             filePaths.map(async (filePath: string): Promise<AudioFile> => {
                 const metadata: IAudioMetadata = await parseBuffer(
-                    fs.readFileSync(filePath),
+                    readFileSync(filePath),
                     filePath
                 );
 
                 let imageBase64 = null;
 
                 if (metadata.common.picture?.length) {
-                    const picture = metadata.common.picture[0];
-                    const base64Image = uint8ArrayToBase64(picture.data);
+                    const picture: IPicture = metadata.common.picture[0];
+                    const base64Image: string = uint8ArrayToBase64(picture.data);
                     imageBase64 = `data:${picture.format};base64,${base64Image}`;
                 }
 
@@ -40,7 +48,7 @@ export function registerFileHandlers() {
                     duration: metadata.format.duration || 0,
                     lyrics: metadata.common.lyrics || [],
                     path: filePath,
-                    image: imageBase64 || null,
+                    image: imageBase64 || null
                 };
             })
         );
@@ -48,8 +56,8 @@ export function registerFileHandlers() {
 
     ipcMain.handle('load-audio-file', async (_, filePath): Promise<string> => {
         try {
-            const audioData = fs.readFileSync(filePath);
-            const mimeType = mime.lookup(filePath) || 'audio/mpeg';
+            const audioData: Buffer = readFileSync(filePath);
+            const mimeType: string = lookup(filePath) || 'audio/mpeg';
             return `data:${mimeType};base64,${audioData.toString('base64')}`;
         } catch (error) {
             console.error('Error loading audio file:', error);
--- electron/preload/index.ts
+++ electron/preload/index.ts
@@ -70,7 +70,7 @@ function useLoading() {
   animation-fill-mode: both;
   width: 50px;
   height: 50px;
-  background: #fff;
+  background: #FF0033;
   animation: square-spin 3s 0s cubic-bezier(0.09, 0.57, 0.49, 0.9) infinite;
 }
 .app-loading-wrap {
@@ -82,7 +82,7 @@ function useLoading() {
   display: flex;
   align-items: center;
   justify-content: center;
-  background: #282c34;
+  background: #181818;
   z-index: 9;
 }
     `
