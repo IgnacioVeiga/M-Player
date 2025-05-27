c0da53fca7a3d6b8b0c21714e4ce10545def0138
Ignacio Veiga <ignacio.veiga29@gmail.com>
2025-05-26 23:30:58 -0300

    Refactor Lyrics.tsx: añade tipos de retorno a la función para mejorar la claridad y la gestión de tipos

--- src/components/Lyrics.tsx
+++ src/components/Lyrics.tsx
@@ -1,6 +1,10 @@
 import { LyricsProps } from "@/types";
+import { JSX } from "react/jsx-runtime";
 
-export default function Lyrics({ file }: LyricsProps) {
+export default function Lyrics({ file }: LyricsProps): JSX.Element {
+	if (!file || !file.lyrics || file.lyrics.length === 0) {
+		return <p className="px-5 py-0 text-center whitespace-break-spaces">No lyrics available</p>;
+	} else {
 	return (
 		<p className="px-5 py-0 text-center whitespace-break-spaces">
 			{file?.lyrics?.map((line, idx) => (
